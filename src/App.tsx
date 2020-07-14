d16bd4263bf8bfdd707dfd3c9383b6651beb1682
kartikmshah <kartik.shah@robosoftin.com>
2020-07-14 22:47:43 +0530

    Initial setup.

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+const App: React.FC = () => {
+  return (
+      <div>Test goes on</div>
+  );
+};
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { SvgIcon } from '@material-ui/core';
+import App from "./App";
+import './styles/index.scss';
+
+ReactDOM.render(<App />, document.getElementById("root"));
